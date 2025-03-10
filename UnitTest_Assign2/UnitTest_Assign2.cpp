e780821dc0586f83d3c70ac6f8c3be4ea256ece3
mou-gits <Msarkar0406@conestogac.on.ca>
2025-03-09 22:40:50 -0400

    Added TASK1 TEST_CLASS to test method getPerimeter

---
+++ UnitTest_Assign2/UnitTest_Assign2.cpp
@@ -0,0 +1,27 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+#include "..\BCSRec\main.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace TASK1
+{
+	TEST_CLASS(TASK1)
+	{
+	public:
+
+		TEST_METHOD(getPerimeter_LenghtWidth_perimetercalculation)
+		{
+			//This function validates getPerimeter 
+
+			int length = 10;
+			int width = 20;
+
+			int Result = getPerimeter(&length, &width);
+			Assert::AreEqual(60, Result);
+		}
+	};
+}
+
+
+
